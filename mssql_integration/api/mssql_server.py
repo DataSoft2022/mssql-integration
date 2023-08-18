f2f7dd85f68b799b565de78384dfeb4a84a44d35
Ahmed Abdulrahman <ahmed751995@gmail.com>
2023-08-18 21:45:15 +0300

    try except returned

--- mssql_integration/api/mssql_server.py
+++ mssql_integration/api/mssql_server.py
@@ -32,29 +32,34 @@ def send_to_mssql(frm):
     engine = setup_db()
     frm = json.loads(frm)
     company = frappe.get_doc('Company', frm.get('company'))
-    with engine.connect() as conn:
-        conn.execute(
-            text(
-                "insert into OBTF (TransId, RefDate, DueDate, WritingDate, Memo) values (:trans_id, :ref_date, :due_date, :writing_date, :memo)"
-            ), {
-                "trans_id": frm.get("name"),
-                "ref_date": frm.get("posting_date"),
-                "due_date": frm.get("creation").split()[0].strip(),
-                "writing_date": frm.get("creation").split()[0].strip(),
-                "memo": frm.get("user_remark")
-            })
-
-        for account in frm.get("accounts"):
+    try:
+        with engine.connect() as conn:
             conn.execute(
                 text(
-                    "insert into BTF1 (TransId, Line_ID, Account, Debit, Credit, FCCurrency) values (:trans_id, :line_id, :account, :debit, :credit, :fc_currency)"
+                    "insert into OBTF (TransId, RefDate, DueDate, WritingDate, Memo) values (:trans_id, :ref_date, :due_date, :writing_date, :memo)"
                 ), {
-                    "trans_id": account.get("name"),
-                    "line_id": account.get("idx"),
-                    "account": account.get("account"),
-                    "debit": account.get("debit"),
-                    "credit": account.get("credit"),
-                    "fc_currency": company.default_currency
+                    "trans_id": frm.get("name"),
+                    "ref_date": frm.get("posting_date"),
+                    "due_date": frm.get("creation").split()[0].strip(),
+                    "writing_date": frm.get("creation").split()[0].strip(),
+                    "memo": frm.get("user_remark")
                 })
+
+            for account in frm.get("accounts"):
+                conn.execute(
+                    text(
+                        "insert into BTF1 (TransId, Line_ID, Account, Debit, Credit, FCCurrency) values (:trans_id, :line_id, :account, :debit, :credit, :fc_currency)"
+                    ), {
+                        "trans_id": account.get("name"),
+                        "line_id": account.get("idx"),
+                        "account": account.get("account"),
+                        "debit": account.get("debit"),
+                        "credit": account.get("credit"),
+                        "fc_currency": company.default_currency
+                    })
             conn.commit()
-    return {"success": True}
+        return {"success": True}
+    except DBAPIError:
+        # print("exception  ", e.__class__.__name__)
+        #frappe.render_form()
+        return {"success": False, "message": "check db configurations"}
