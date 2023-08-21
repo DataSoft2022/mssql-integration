0b6b2a2857da6f5b78b5ed2f9bb553ddd7c110f6
Ahmed Abdulrahman <ahmed751995@gmail.com>
2023-08-21 23:55:17 +0300

    send to sap button

--- mssql_integration/mssql_integration/doctype/mssql_configurations/mssql_configurations.js
+++ mssql_integration/mssql_integration/doctype/mssql_configurations/mssql_configurations.js
@@ -7,17 +7,26 @@ frappe.ui.form.on("Mssql Configurations", {
 });
 
 frappe.ui.form.on("Journal Entry", {
-  on_submit: async function (frm) {
-    let resp = await frappe.call({
-      method: "mssql_integration.api.mssql_server.send_to_mssql",
-      type: "POST",
-      async: false,
-      args: {
-        frm: frm.doc,
-      },
-    });
-    if (!resp.message.success) {
-      frm.reload_doc().then(() => frappe.throw(resp.message.message));
+  refresh(frm) {
+    if (frm.doc.docstatus) {
+      frm.add_custom_button(__("Send to SAP"), function () {
+        frappe.msgprint(__("Sending to SAP ....."));
+        let resp = frappe.call({
+          method: "mssql_integration.api.mssql_server.send_to_mssql",
+          type: "POST",
+          async: false,
+          args: {
+            frm: frm.doc,
+          },
+          callback(resp) {
+            if (!resp.message.success) {
+              frm.reload_doc().then(() => frappe.throw(resp.message.message));
+            } else {
+              frappe.hide_msgprint();
+            }
+          },
+        });
+      });
     }
   },
 });
